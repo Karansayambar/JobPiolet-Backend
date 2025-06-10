62c0764d9e58a58d1b9054f2870e82f6b98ef0bd
SayambarKaran <karan.subhashsayambar@quadbtech.com>
2025-06-10 18:11:06 +0530

    made changes in post job

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://job-piolet-frontend.vercel.app",
+    // origin: "https://job-piolet-frontend.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true,
   })
